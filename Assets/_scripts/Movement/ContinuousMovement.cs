85a67f10229a490dc81746a77c50670955bc8007
Aurimas Bruzgys <55162517+Ranteidz@users.noreply.github.com>
2022-11-18 19:19:06 +0100

    Player movement, added gravity to player

--- Assets/_scripts/Movement/ContinuousMovement.cs
+++ Assets/_scripts/Movement/ContinuousMovement.cs
@@ -12,6 +12,9 @@ namespace _Scripts
         private CharacterController _character;
         private Vector2 _inputAxis;
         private XROrigin _rig;
+        public float gravity = -9.81f;
+        private float _fallingSpeed;
+        public LayerMask groundLayer;
        
         private void Start()
         {
@@ -32,6 +35,19 @@ namespace _Scripts
             var direction = headYaw * new Vector3(_inputAxis.x, 0, _inputAxis.y);
 
             _character.Move(direction * Time.fixedDeltaTime * speed);
+
+            bool isGrounded = checkIfGrounded();
+
+            if (isGrounded)
+            {
+                _fallingSpeed = 0;
+            }
+            else
+            {
+                _fallingSpeed += gravity * Time.fixedDeltaTime;
+            }
+            
+            _character.Move(Vector3.up * _fallingSpeed * Time.fixedDeltaTime);
         }
 
         private void FollowHeadset()
@@ -40,6 +56,15 @@ namespace _Scripts
             var capsuleCenter = transform.InverseTransformPoint(_rig.Origin.transform.position);
             _character.center =
                 new Vector3(capsuleCenter.x, _character.height / 2 + _character.skinWidth, capsuleCenter.z);
+        }
+
+        private bool checkIfGrounded()
+        {
+            Vector3 rayStart = transform.TransformPoint(_character.center);
+            float rayLenght = _character.center.y + 0.01f;
+            bool hasHit = Physics.SphereCast(rayStart, _character.radius, Vector3.down, out RaycastHit hitInfo,
+                rayLenght, groundLayer);
+            return hasHit;
         }
     }
 }
