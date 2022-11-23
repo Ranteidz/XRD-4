3afc763102670ce4f0ac130e7fca92e8c6cd57c8
Aurimas Bruzgys <55162517+Ranteidz@users.noreply.github.com>
2022-11-23 01:55:54 +0100

    Imported NavMesh components, WIP for enemy states

--- Assets/_scripts/Enemy/Follow_Enemy.cs
+++ Assets/_scripts/Enemy/Follow_Enemy.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine.AI;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Follow_Enemy : MonoBehaviour
 {
@@ -13,6 +15,18 @@ public class Follow_Enemy : MonoBehaviour
     public Transform[] wayPoints;
     
     public States currentState;
+    [Header("Shooting")] 
+    public Transform shootingPoint;
+
+    public float shootDistance = 10f;
+    private bool inSight;
+    private Vector3 directionToTarget;
+
+    [Header("Weapon")] 
+    public float fireRate = 2f;
+    public float velocity;
+    public GameObject projectile;
+    private bool _canShoot;
 
     private int currentWayPoint;
     // Start is called before the first frame update
@@ -28,6 +42,9 @@ public class Follow_Enemy : MonoBehaviour
     void Update()
     {
         UpdateStates();
+        CheckIfTargetInRangeForFollow();
+
+        StartCoroutine(StartAttack());
     }
 
     private void UpdateStates()
@@ -47,6 +64,20 @@ public class Follow_Enemy : MonoBehaviour
         }
     }
 
+    private void CheckIfTargetInRangeForFollow()
+    {
+        directionToTarget = target.position - transform.position;
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, directionToTarget.normalized, out hit))
+        {
+            inSight = hit.transform.CompareTag("Player");
+        }
+        
+        currentState = States.Follow;
+    }
+
     private void Patrol()
     {
         if (agent.destination != wayPoints[currentWayPoint].position)
@@ -62,14 +93,53 @@ public class Follow_Enemy : MonoBehaviour
     }
     private void Follow()
     {
-        if (target != null)
+        if (agent.remainingDistance <= shootDistance && inSight)
         {
-            agent.SetDestination(target.position);
+            agent.ResetPath();
+            /*currentState = States.Attack;*/
+        }
+        else
+        {
+            if (target != null)
+            {
+                agent.SetDestination(target.position);
+            }    
         }
     }
     private void Attack()
     {
+        if (!inSight)
+        {
+            currentState = States.Follow;
+        }
+        LookAtTarget();
+        StartCoroutine(StartAttack());
+    }
+
+    private void Shoot()
+    {
+        var spawnedProjectile = Instantiate(projectile, shootingPoint.position, Quaternion.identity);
+        var moveDirection = (target.transform.position - spawnedProjectile.transform.position).normalized;
+
+        spawnedProjectile.GetComponent<Rigidbody>().velocity = velocity * moveDirection;
+    }
+
+    private IEnumerator StartAttack()
+    {
+        _canShoot = false;
+        Shoot();
+        yield return new WaitForSeconds(fireRate);
+        _canShoot = true;
+    }
+
+    private void LookAtTarget()
+    {
+        Vector3 lookDirection = directionToTarget;
+        lookDirection.y = 0f;
+
+        Quaternion lookRotation = Quaternion.LookRotation(lookDirection);
         
+        transform.rotation = Quaternion.Lerp(transform.rotation, lookRotation, Time.deltaTime * agent.angularSpeed);
     }
 
     private bool HasReached()
